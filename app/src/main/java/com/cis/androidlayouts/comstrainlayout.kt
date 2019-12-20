25f2385a64c157071b7d2c6ece4d5f0fb62b4dc1
wongskorn <58836875+wongskorn@users.noreply.github.com>
2019-12-20 12:03:33 +0700

    Layout

---
+++ app/src/main/java/com/cis/androidlayouts/comstrainlayout.kt
@@ -0,0 +1,16 @@
+package com.cis.androidlayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_comstrainlayout.*
+
+class comstrainlayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comstrainlayout)
+
+
+
+    }
+}
